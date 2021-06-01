d4228c1c6b1a745f07cbeb8a91adc6b01f058338
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-06-01 19:07:26 +0530

    Add files via upload

---
+++ 04-Accordion/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+
+
+  const [questions, useQuestions] = useState(data);
+
+  return <main>
+    <div className='container'>
+      <h3>Questions and answers about login</h3>
+      <section className="info">
+        {questions.map((question) => {
+          return <SingleQuestion
+            key={question.id}
+            {...question}
+          />
+        })}
+      </section>
+
+    </div>
+
+  </main>;
+}
+
+export default App;
